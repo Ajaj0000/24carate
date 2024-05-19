5862392e321e0b779b7e90cebf9972c76b6cce4e
Ajaj0000 <ajajkhan2842@gmail.com>
2024-05-19 23:30:05 +0530

    first commit

---
+++ src/Header/header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+    const home = useNavigate()
+    const click = ()=>{
+        home('/')
+    }
+    return (
+        <>
+            <div className="header">
+                <div className="nav1">
+                    <ul>
+                        <li>ABOUT <ul className="menu">
+                            <li>ABOUT 24 CARROTS</li>
+                            <li>TEAM</li>
+                            <li>AWARDS & PRESS</li>
+                            <li>BLOG</li>
+                        </ul></li>
+
+                        <li>SERVICES <ul className="menu">
+                            <li>GOURMET CATERING</li>
+                            <li>BAR SERVICE</li>
+                            <li>STAFFING</li>
+                            <li>PRODUCTION</li>
+                        </ul></li>
+
+                        <li>EVENTS <ul className="menu">
+                            <li>WEDDINGS</li>
+                            <li>CORPORATE</li>
+                            <li>SOCIAL</li>
+                            <li>GALLERY</li>
+                        </ul></li>
+                    </ul>
+                </div>
+                <div style={{cursor:"pointer"}} onClick={click} className="logo">
+                    <h1>24 CARROTS</h1>
+                    <p>CATERING & EVENTS</p>
+                </div>
+                <div className="nav2">
+                    <ul>
+                        <li>VENUES</li>
+                        <li>CAREERS</li>
+                        <li className="bu">GET IN TOUCH</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+export { Header }
